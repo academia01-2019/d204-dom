57b679c1a413cfa2690ab80ee8e480c216525b03
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-02-14 12:37:58 -0200

    Adicionar exemplo de Menu Hamburguer

--- scripts.js
+++ scripts.js
@@ -1,20 +1,30 @@
-const quadrado = document.querySelector('.quadrado');
-const imgFaustao = document.querySelector('img');
+// const quadrado = document.querySelector('.quadrado');
+// const imgFaustao = document.querySelector('img');
 
-const mostrarQuadrado = function () {
-  quadrado.classList.toggle('visivel');
-}
-
-const destaqueImagem = function () {
-  imgFaustao.classList.toggle('destaque');
-}
-
-// quadrado.onmouseover = function () {
+// const mostrarQuadrado = function () {
 //   quadrado.classList.toggle('visivel');
 // }
 
-quadrado.onmouseenter = mostrarQuadrado;
-quadrado.onmouseout = mostrarQuadrado;
+// const destaqueImagem = function () {
+//   imgFaustao.classList.toggle('destaque');
+// }
 
-imgFaustao.onmouseenter = destaqueImagem;
-imgFaustao.onmouseout = destaqueImagem;
+// // quadrado.onmouseover = function () {
+// //   quadrado.classList.toggle('visivel');
+// // }
+
+// quadrado.onmouseenter = mostrarQuadrado;
+// quadrado.onmouseout = mostrarQuadrado;
+
+// imgFaustao.onmouseenter = destaqueImagem;
+// imgFaustao.onmouseout = destaqueImagem;
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const navMenuHamburguer = document.querySelector('.nav-menu-hamburguer');
+
+const atualizarMenu = function () {
+  menuHamburguer.classList.toggle('ativo');
+  navMenuHamburguer.classList.toggle('visivel');
+}
+
+menuHamburguer.onclick = atualizarMenu;
